23d26822c384ec40c3895b9f77d28b1868011e56
CoreOS Admin <core@sgclient-docker-dal09.casenation.poc>
2016-09-23 00:21:25 +0000

    automate master-master replication

--- mysql/add_login.sh
+++ mysql/add_login.sh
@@ -20,7 +20,7 @@ for param in $@; do
           HOST=`echo ${param}  | cut -d= -f2`
           ;;
       --user=*)
-          USER=`echo ${param}  | cut -d= -f2`
+          USERNAME=`echo ${param}  | cut -d= -f2`
           ;;
 
       *)
@@ -39,8 +39,8 @@ if [ -z "${PASSWORD}" ]; then
     return 1
 fi
 
-if [ -z "${USER}" ]; then
-    USER=root
+if [ -z "${USERNAME}" ]; then
+    USERNAME=root
 fi
 
 if [ -z "${PORT}" ]; then
@@ -48,15 +48,16 @@ if [ -z "${PORT}" ]; then
 fi
 
 if [ ! -z "${HOST}" ]; then
-    login_path=${USER}_`echo ${HOST} | sed -e 's/\./-/g'`_${PORT}
+    login_path=${USERNAME}_`echo ${HOST} | sed -e 's/\./-/g'`_${PORT}
     host_port="--host=${HOST} --port=${PORT}"
 else
-    login_path=${USER}
+    login_path=${USERNAME}
 fi
 
-echo ${PASSWORD} | ${DOCKER_CMD} exec -i ${CONTAINER_NAME} mysql_config_editor set --login-path=${login_path} ${host_port} --user=${USER} --password
+${DOCKER_CMD} exec -i ${CONTAINER_NAME} mysql_config_editor remove --login-path=${login_path} 
+echo ${PASSWORD} | ${DOCKER_CMD} exec -i ${CONTAINER_NAME} mysql_config_editor set --login-path=${login_path} ${host_port} --user=${USERNAME} --password
 
-echo ${login_path}
+echo "${login_path}"
 
 
 
