928511ef6130761cc55a887dfb143a32e2a192b2
jkwong888 <jkwong888@users.noreply.github.com>
2016-09-27 14:57:58 -0400

    mysql cluster automation (#3)
    * add scripts to build up mysql cluster on two different hosts in docker containers
    
    * automate master-master replication
    
    * mysql cluster image and scripts
    
    * fix script error
    
    * start ndbd docker containers
    
    * add binary logging, expose ports for sql nodes
    
    * add load-data scripts
    
    * distribute privileges for ndbcluster
    
    * fix distributed permissions

---
+++ mysql-cluster/build_mysql_cluster.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+
+. ./env.sh
+
+NODE_TYPE=$1
+NODE_IDX=$2
+
+function print_usage {
+    echo "$0 <mgmt|sql|data> <idx>"
+    echo
+    echo "configured cluster size:"
+    echo "Mgmt nodes: ${num_mgmt_nodes}"
+    echo "Sql nodes: ${num_sql_nodes}"
+    echo "Data nodes: ${num_data_nodes}"
+}
+
+function get_ip4 {
+    _subnet=$1
+    _offset=$2
+
+    _network=`echo ${_subnet} | cut -d/ -f1`
+    IFS=. read -r _octet1 _octet2 _octet3 _octet4 <<< "${_network}"
+
+    _cidr=`echo ${_subnet} | cut -d/ -f2`
+    _long_network=`printf '%d\n' "$((_octet1 * 256 ** 3 + _octet2 * 256 ** 2 + _octet3 * 256 + _octet4))"`
+
+    for i in `seq 1 ${_cidr}`; do
+        _str="${_str}1"
+    done
+    
+    for i in `seq ${_cidr} 32`; do
+        _str="${_str}0"
+    done
+    
+    _mask=$((2#${_str}))
+    
+    _long_ip=$(((${_long_network} & ${_mask}) + ${_offset}))
+    
+    for i in {3..0}; do
+        ((octet = ${_long_ip} / (256 ** i) ))
+        ((_long_ip -= octet * 256 ** i ))
+        ip=${ip}${delim}${octet}
+        delim=.
+    done
+    
+    
+    echo ${ip}
+}
+
+
+if [ -z "${NODE_TYPE}" ]; then
+    echo "NODE_TYPE not set"
+    print_usage
+    exit 1
+fi
+
+if [ -z "${NODE_IDX}" ]; then
+    echo "NODE_IDX not set"
+    print_usage
+    exit 1
+fi
+
+if [ "${NODE_TYPE}" != "mgmt" -a \
+     "${NODE_TYPE}" != "sql" -a \
+     "${NODE_TYPE}" != "data" ]; then
+    echo "Invalid NODE_TYPE set: ${NODE_TYPE}"
+    print_usage
+    exit 1
+fi
+
+# get dependency scripts from mysql repo
+if [ ! -d "./refarch-cloudnative-mysql" ]; then
+    git clone https://github.com/ibm-cloud-architecture/refarch-cloudnative-mysql.git
+fi
+
+# build network
+docker network inspect mynet
+_rc=$?
+if [ ${_rc} -ne 0 ]; then
+    docker network create -d overlay --subnet=${MY_SUBNET} mynet
+fi
+
+# build docker image
+docker build -t mysql-cluster .
+
+# the IPs will be set up like this, mgmtnode is mynet+2, 
+# sqlnode is mynet+3,mynet+4,
+# and datanodes are mynet+5 and mynet+6 and mynet+7
+
+_ip_idx=2
+MGMT_NODE_IP=`get_ip4 ${MY_SUBNET} $((_ip_idx))`
+SQL_NODE_IPS=""
+DATA_NODE_IPS=""
+_ip_idx=$((_ip_idx+1))
+
+for i in `seq 1 ${num_sql_nodes}`; do
+    SQL_NODE_IPS="${SQL_NODE_IPS};`get_ip4 ${MY_SUBNET} ${_ip_idx}`"
+    _ip_idx=$((_ip_idx+1))
+done
+
+for i in `seq 1 ${num_data_nodes}`; do
+    DATA_NODE_IPS="${DATA_NODE_IPS};`get_ip4 ${MY_SUBNET} ${_ip_idx}`"
+    _ip_idx=$((_ip_idx+1))
+done
+
+# write out config.ini
+cat > config.ini <<ENDCONFIGINI
+[ndbd default]
+NoOfReplicas=${num_data_nodes}
+DataMemory=80M
+IndexMemory=18M
+
+[ndb_mgmd]
+hostname=${MGMT_NODE_IP}
+datadir=/var/lib/mysql-cluster
+
+ENDCONFIGINI
+
+for SQL_NODE_IP  in `echo ${SQL_NODE_IPS} | sed -e 's/;/ /g'`; do
+    echo "[mysqld]" >> config.ini
+    echo "hostname=${SQL_NODE_IP}" >> config.ini
+    echo "" >> config.ini
+done
+
+for DATA_NODE_IP in `echo ${DATA_NODE_IPS} | sed -e 's/;/ /g'`; do
+    echo "[ndbd]" >> config.ini
+    echo "hostname=${DATA_NODE_IP}" >> config.ini
+    echo "datadir=/var/lib/mysql/data" >> config.ini
+    echo "" >> config.ini
+done
+
+cat > my.cnf <<ENDMYCNF
+[client]
+port=3306
+socket=/var/lib/mysql/mysql.sock
+
+[mysqld]
+port=3306
+datadir=/var/lib/mysql
+socket=/var/lib/mysql/mysql.sock
+user=mysql
+# Disabling symbolic-links is recommended to prevent assorted security risks
+symbolic-links=0
+ndbcluster=1
+
+log-bin=mysql-bin
+binlog-format=row
+server-id=${NODE_IDX}${SITE_MASK}
+auto-increment-increment = 2
+auto-increment-offset = $((SITE_MASK+1))
+replicate-same-server-id = 0
+
+[mysql_cluster]
+ndb-connectstring=${MGMT_NODE_IP}
+
+[mysqld_safe]
+log-error=/var/log/mysqld.log
+pid-file=/var/run/mysqld/mysqld.pid
+ENDMYCNF
+
+if [ "${NODE_TYPE}" == "mgmt" ]; then
+    # stop and delete any mgmt nodes
+    docker stop mysql-${SITE_NAME}-mgmtnode${NODE_IDX}
+    docker rm mysql-${SITE_NAME}-mgmtnode${NODE_IDX}
+
+    # start mgmt node
+    docker run -d --net mynet --ip ${MGMT_NODE_IP} -v `pwd`/config.ini:/etc/mysql-cluster.ini --name mysql-${SITE_NAME}-mgmtnode${NODE_IDX} mysql-cluster ndb_mgmd
+    
+elif [ "${NODE_TYPE}" == "sql" ]; then
+    # stop and delete any sql nodes
+    _count=1
+    for SQL_NODE_IP  in `echo ${SQL_NODE_IPS} | sed -e 's/;/ /g'`; do
+        if [ ! -z "${NODE_IDX}" -a \
+             "${NODE_IDX}" == "${_count}" ]; then
+            docker stop mysql-${SITE_NAME}-sqlnode${_count}
+            docker rm mysql-${SITE_NAME}-sqlnode${_count}
+
+            node_ip=`hostname -i`
+            docker run -d --net mynet -p ${node_ip}:3306:3306 --ip ${SQL_NODE_IP} -v `pwd`/my.cnf:/etc/my.cnf --name mysql-${SITE_NAME}-sqlnode${_count} mysql-cluster mysqld
+
+        fi
+        _count=$((_count+1))
+    done
+elif [ "${NODE_TYPE}" == "data" ]; then
+    # stop and delete any data nodes
+    _count=1
+    for DATA_NODE_IP  in `echo ${DATA_NODE_IPS} | sed -e 's/;/ /g'`; do
+        if [ ! -z "${NODE_IDX}" -a \
+             "${NODE_IDX}" == "${_count}" ]; then
+
+            docker stop mysql-${SITE_NAME}-datanode${_count}
+            docker rm mysql-${SITE_NAME}-datanode${_count}
+
+            docker run -d --net mynet --ip ${DATA_NODE_IP} -v `pwd`/my.cnf:/etc/my.cnf --name mysql-${SITE_NAME}-datanode${_count} mysql-cluster ndbd
+        fi
+        _count=$((_count+1))
+    done
+fi
+
+
---
+++ mysql-cluster/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CMD="$@"
+
+if [ "$1" == "mysqld" ]; then
+
+    /usr/local/mysql/scripts/mysql_install_db --user=mysql --basedir=/usr/local/mysql --datadir=/var/lib/mysql
+
+
+elif [ "$1" == "ndb_mgmd" ]; then
+    ARGS="-f /etc/mysql-cluster.ini --nodaemon"
+elif [ "$1" == "ndbd" ]; then
+    ARGS="--nodaemon"
+fi
+
+exec "$CMD" $ARGS 
+
+
+sleep 10
+
+if [ "$1" == "mysqld" ]; then
+    # distribute the privileges
+    mysql -e 'source /usr/local/mysql/share/ndb_dist_priv.sql'
+    mysql -e 'CALL mysql.mysql_cluster_move_privileges();'
+fi
